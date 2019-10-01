e59f52d8b2763dce75d1dda595c416190f6683be
allatif <omar.al-latif@hochschule-stralsund.de>
2019-10-01 08:22:50 +0200

    added function get_pp() to color.py - rgba values can be transformed into values within 0-1 range, needed for pyplot

--- data/components/color.py
+++ data/components/color.py
@@ -8,7 +8,10 @@ DGREY = (64, 64, 64)
 
 RED = (225, 0, 0)
 GREEN = (0, 225, 0)
-BLUE = (0, 0, 255)
+BLUE = (0, 0, 225)
+
+ORANGE = (255, 127, 80)
+TOMATO = (255, 99, 71)
 
 LLRED = (228, 88, 88)
 LRED = (200, 55, 55)
@@ -17,9 +20,6 @@ DRED = (125, 0, 0)
 LBLUE = (30, 144, 255)
 DBLUE = (0, 0, 128)
 DDBLUE = (0, 0, 45)
-
-ORANGE = (255, 127, 80)
-TOMATO = (255, 99, 71)
 
 # ALPHA COLORS (RGBA)
 _RGB = (0, 0, 0)
@@ -31,3 +31,19 @@ ARED = (*RED, 128)
 ADDBLUE = (*DDBLUE, 128)
 
 A200DDBLUE = (*DDBLUE, 200)
+
+
+# COLORS FOR PYPLOT
+
+
+def get_pp(color):
+    """Returns a RGBA tuple with values within 0-1 range
+
+    Needed for applying colors in pyplot.
+    """
+
+    _range = 255
+    _rgba = []
+    for e in color:
+        _rgba.append(e/_range)
+    return tuple(_rgba)
