a4ae652df7cdc8bced729d437e5b845b9f180cb1
allatif <omar.al-latif@hochschule-stralsund.de>
2020-05-05 08:44:54 +0200

    successfully trained first model in testing_rl.py

--- testing_rl.py
+++ testing_rl.py
@@ -1,13 +1,14 @@
-# Just disables the warning, doesn't enable AVX/FMA
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-from time import sleep
 
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
 
 from data.components.rl import environment as env
+
+
+# Just disables the warning, doesn't enable AVX/FMA
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 n_inputs = 4
@@ -16,20 +17,80 @@ model = keras.models.Sequential([
     keras.layers.Dense(1, activation="sigmoid"),
 ])
 
-"""
+
 env = env.Environment()
-
 obs = env.reset()
-print(obs)
 
-for s in range(200):
-    action = 1
-    obs, reward, done = env.step(action)
-    # print(obs)
-    # print("reward:", reward)
-    # print(done)
-    sleep(0.02)
-    if done:
-        print(s)
-        break
-"""
+
+def play_one_step(env, obs, model, loss_fn):
+    with tf.GradientTape() as tape:
+        left_proba = model(obs[np.newaxis])
+        action = (tf.random.uniform([1, 1]) > left_proba)
+        y_target = tf.constant([[1.]]) - tf.cast(action, tf.float32)
+        loss = tf.reduce_mean(loss_fn(y_target, left_proba))
+    grads = tape.gradient(loss, model.trainable_variables)
+    obs, reward, done = env.step(int(action[0, 0].numpy()))
+    return obs, reward, done, grads
+
+
+def play_multiple_episodes(env, n_eps, n_max_steps, model, loss_fn):
+    all_rewards = []
+    all_grads = []
+    for ep in range(n_eps):
+        current_rewards = []
+        current_grads = []
+        obs = env.reset()
+        for step in range(n_max_steps):
+            obs, reward, done, grads = play_one_step(env, obs, model, loss_fn)
+            current_rewards.append(reward)
+            current_grads.append(grads)
+            if done:
+                break
+        all_rewards.append(current_rewards)
+        all_grads.append(current_grads)
+    return all_rewards, all_grads
+
+
+def discount_rewards(rewards, discount_factor):
+    discounted = np.array(rewards)
+    for step in range(len(rewards) - 2, -1, -1):
+        discounted[step] += discounted[step + 1] * discount_factor
+    return discounted
+
+
+def discount_and_normalize_rewards(all_rewards, discount_factor):
+    all_discounted_rewards = [discount_rewards(rewards, discount_factor)
+                              for rewards in all_rewards]
+    flat_rewards = np.concatenate(all_discounted_rewards)
+    reward_mean = flat_rewards.mean()
+    reward_std = flat_rewards.std()
+    return [(discount_rewards - reward_mean) / reward_std
+            for discount_rewards in all_discounted_rewards]
+
+
+n_iterations = 150
+n_eps_per_update = 10
+n_max_steps = 200
+discount_factor = 0.95
+
+optimizer = keras.optimizers.Adam(lr=0.01)
+loss_fn = keras.losses.binary_crossentropy
+
+for iteration in range(n_iterations):
+    print("start iter num:", iteration+1, "of total", n_iterations)
+    all_rewards, all_grads = play_multiple_episodes(
+        env, n_eps_per_update, n_max_steps, model, loss_fn)
+    all_final_rewards = discount_and_normalize_rewards(all_rewards,
+                                                       discount_factor)
+
+    all_mean_grads = []
+    for var_index in range(len(model.trainable_variables)):
+        mean_grads = tf.reduce_mean(
+            [final_reward * all_grads[episode_index][step][var_index]
+             for episode_index, final_rewards in enumerate(all_final_rewards)
+             for step, final_reward in enumerate(final_rewards)], axis=0)
+        all_mean_grads.append(mean_grads)
+
+    optimizer.apply_gradients(zip(all_mean_grads, model.trainable_variables))
+
+model.save("sphere_cone_rl_pg.h5")
