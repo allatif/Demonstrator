cf0accd5cd9fb25d44b6db35c0f69a43c62ecbfa
allatif <omar.al-latif@hochschule-stralsund.de>
2020-05-07 13:27:09 +0200

    restructured Agent class, added 2 new methods: update(), observe()

--- data/components/rl/agent.py
+++ data/components/rl/agent.py
@@ -1,7 +1,8 @@
 import os
 
 import numpy as np
-import tensorflow as tf
+
+print(" -- Loading Tensorflow -- ")
 from tensorflow import keras
 
 
@@ -11,17 +12,34 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 class Agent:
 
-    def __init__(self):
-        pass
+    def __init__(self, sensibility=10000):
+        self._sens = sensibility
+
+        self._force = 0.0
 
     def load_model(self, name):
         path = 'data\\components\\rl\\models\\' + name
         self.model = keras.models.load_model(path)
+        print("Finished loading model")
 
     def act(self, obs):
         left_proba = self.model.predict(obs.reshape(1, -1))
         action = int(np.random.rand() > left_proba)
         if action == 0:
-            return -10000
+            return -self._sens
         elif action == 1:
-            return 10000
+            return self._sens
+
+    def observe(self, obs):
+        self._obs = obs
+
+    def update(self):
+        self._force = self.act(self._obs)
+
+    @property
+    def force(self):
+        return self._force
+
+    @property
+    def obs(self):
+        return self._obs
