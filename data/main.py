380a10fd0be2d02a9026f5b7ad5af66f8a5797fd
allatif <omar.al-latif@hochschule-stralsund.de>
2020-05-12 12:57:18 +0200

    added new state 'MAINSETTINGS' to state_dict

--- data/main.py
+++ data/main.py
@@ -1,11 +1,12 @@
 from . import pg_init, pg_root
-from . states import splash, polemap, neuro, game, menu
+from . states import splash, main_menu, polemap, neuro, game, menu
 
 
 def main():
     """Add states to control here."""
     pgapp = pg_root.PygameApp(pg_init.ORIGINAL_CAPTION)
     state_dict = {"SPLASH": splash.Splash(),
+                  "MAINSETTINGS": main_menu.MainSetup(),
                   "POLEMAP": polemap.PoleMap(),
                   "NEURO": neuro.Neuro(),
                   "GAME": game.Game(),
