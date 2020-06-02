c3e3d2d1771d47ccc67fbee0f7e99b5d3d5a90d3
allatif <omar.al-latif@hochschule-stralsund.de>
2020-06-02 16:32:58 +0200

    added new testing script for environment

---
+++ testing_env.py
@@ -0,0 +1,15 @@
+from data.components.rl import environment as env
+
+
+env = env.Environment()
+obs = env.reset()
+print(obs)
+
+action = 0
+
+for step in range(100):
+    obs, r, done = env.step(action)
+    print(step+1, obs)
+    if done:
+        print('-> Failed at step', step+1)
+        break
