f4e15973e94f675be7fd9e873cccccb207eac210
allatif <omar.al-latif@hochschule-stralsund.de>
2020-06-02 16:32:15 +0200

    code maintenance / relocated tool functions into util modul that were used before directly in the training script modul

---
+++ data/components/rl/util.py
@@ -0,0 +1,59 @@
+import os
+
+import tensorflow as tf
+import numpy as np
+
+
+# Just disables the warning, doesn't enable AVX/FMA
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+def play_one_step(env, obs, model, loss_fn):
+    with tf.GradientTape() as tape:
+        left_proba = model(obs[np.newaxis])
+        action = (tf.random.uniform([1, 1]) > left_proba)
+        y_target = tf.constant([[1.]]) - tf.cast(action, tf.float32)
+        loss = tf.reduce_mean(loss_fn(y_target, left_proba))
+    grads = tape.gradient(loss, model.trainable_variables)
+    obs, reward, done = env.step(int(action[0, 0].numpy()))
+    return obs, reward, done, grads, loss
+
+
+def play_multiple_episodes(env, n_eps, n_max_steps, model, loss_fn):
+    all_rewards = []
+    all_grads = []
+    total_loss = 0
+    for ep in range(n_eps):
+        current_rewards = []
+        current_grads = []
+        episode_loss = 0
+        obs = env.reset()
+        for step in range(n_max_steps):
+            obs, reward, done, grads, loss = play_one_step(env, obs,
+                                                           model, loss_fn)
+            current_rewards.append(reward)
+            current_grads.append(grads)
+            episode_loss += loss
+            if done:
+                break
+        all_rewards.append(current_rewards)
+        all_grads.append(current_grads)
+        total_loss += episode_loss
+    return all_rewards, all_grads, total_loss
+
+
+def discount_rewards(rewards, discount_factor):
+    discounted = np.array(rewards)
+    for step in range(len(rewards) - 2, -1, -1):
+        discounted[step] += discounted[step + 1] * discount_factor
+    return discounted
+
+
+def discount_and_normalize_rewards(all_rewards, discount_factor):
+    all_discounted_rewards = [discount_rewards(rewards, discount_factor)
+                              for rewards in all_rewards]
+    flat_rewards = np.concatenate(all_discounted_rewards)
+    reward_mean = flat_rewards.mean()
+    reward_std = flat_rewards.std()
+    return [(discount_rewards - reward_mean) / reward_std
+            for discount_rewards in all_discounted_rewards]
