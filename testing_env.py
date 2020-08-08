95e445ee2c3341bac1450e4e51707015d2bfbd54
allatif <omar.al-latif@hochschule-stralsund.de>
2020-08-08 17:00:31 +0200

    minor changes in testing_env.py

--- testing_env.py
+++ testing_env.py
@@ -9,7 +9,7 @@ action = 0
 
 for step in range(100):
     obs, r, done = env.step(action)
-    print(step+1, obs)
+    print(step+1, obs, r)
     if done:
         print('-> Failed at step', step+1)
         break
