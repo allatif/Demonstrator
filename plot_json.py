8021e4b6401b763eae5e15e40e00a0cf4f218d09
allatif <omar.al-latif@hochschule-stralsund.de>
2020-08-08 21:19:27 +0200

    changed plot labels

--- plot_json.py
+++ plot_json.py
@@ -12,8 +12,8 @@ total_reward_progress = json.load(open(jsonpath, 'r'))
 # Plot Loss and Rewards
 plt.figure(figsize=(12, 8), dpi=80, facecolor=(colors.get_pp(colors.WHITE)))
 plt.plot(total_reward_progress, color=colors.get_pp(colors.TOMATO))
-plt.xlabel('Episoden')
-plt.ylabel('Gesamt-Rewards')
+plt.xlabel('Episode')  # Iteration
+plt.ylabel('Total rewards')
 plt.grid(True)
 
 plt.savefig(f"{modelname}.png")
