31d8d756aa27a11d66bb6ffcd276612af43ca4f7
allatif <omar.al-latif@hochschule-stralsund.de>
2020-08-15 02:59:11 +0200

    chagned due usage

--- plot_json.py
+++ plot_json.py
@@ -5,7 +5,7 @@ from data.components import colors
 
 
 # Load modelname and rewards from json
-modelname = 'deepq_cartpole_s200_ep600'
+modelname = 'deepq3216_b64_s200_ep1000(100)_sc200'
 jsonpath = 'tools\\conserved_plots\\' + modelname + '.json'
 total_reward_progress = json.load(open(jsonpath, 'r'))
 
