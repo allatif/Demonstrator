0c319085bf09a4532c303b8ef6b65cede4214f8d
allatif <omar.al-latif@hochschule-stralsund.de>
2020-08-31 11:12:34 +0200

    code maintenance / added option to continue old training session

--- pg_training.py
+++ pg_training.py
@@ -8,6 +8,7 @@ import gym
 
 from data.components.rl import environment as env_
 from data.components.rl.pg_util import *
+from data.components.rl import model_ripper as rip
 
 
 # Just disables the warning, doesn't enable AVX/FMA
@@ -48,6 +49,10 @@ def argparser():
                         help="discount factor gamma, default: 0.99",
                         type=float)
 
+    parser.add_argument("-c", "--continue_", metavar="",
+                        help="loads an old model and continue training",
+                        type=str)
+
     parser.add_argument("-a", "--adv_reward",
                         help="use environment with advanced reward function",
                         action="store_true")
@@ -79,14 +84,51 @@ def get_ann(inputs=4, shape=[5]):
 def main():
     args = argparser()
 
-    if not args.openai_gym:
+    hypers = {
+        'iterations': 600,
+        'episodes per iteration': 25,
+        'steps per episode': 200,
+        'discount': 0.99,
+        'advanced reward': args.adv_reward,
+        'reference': args.reference,
+        'variance': 0
+    }
+
+    old_i = 0
+    old_best = 0
+
+    if not args.openai_gym and args.continue_ is None:
         model = get_ann(inputs=args.inputs, shape=args.shape)
 
-    variance = 0
+    if args.continue_:
+        model = keras.models.load_model(args.continue_, compile=False)
+        ripped_hypers = rip.model_ripper(args.continue_)
+        hypers['episodes per iteration'] = ripped_hypers['eps']
+        hypers['steps per episode'] = ripped_hypers['s']
+        hypers['discount'] = ripped_hypers['g']
+        hypers['advanced reward'] = ripped_hypers['R']
+        hypers['reference'] = ripped_hypers['ref']
+        hypers['variance'] = ripped_hypers['var']
+        hypers['info'] = ripped_hypers['info']
+
+        old_i = ripped_hypers['i']
+        old_best = ripped_hypers['Gt']
+
+    # Overwrite hypers when args are passed
+    if args.iterations is not None:
+        hypers['iterations'] = args.iterations
+    if args.episodes is not None:
+        hypers['episodes per iteration'] = args.episodes
+    if args.episode_length is not None:
+        hypers['steps per episode'] = args.episode_length
+    if args.gamma is not None:
+        hypers['discount'] = args.gamma
     if args.variance is not None:
-        variance = args.variance
-    env = env_.Environment(adv_reward=args.adv_reward, ref=args.reference,
-                           variance=variance)
+        hypers['variance'] = args.variance
+
+    env = env_.Environment(adv_reward=hypers['advanced reward'],
+                           ref=hypers['reference'],
+                           variance=hypers['variance'])
     _ = env.reset()
 
     if args.openai_gym:
@@ -94,56 +136,48 @@ def main():
         env = gym.make("CartPole-v1")
         _ = env.reset()
 
-    # number of training iterations
-    n_iterations = 600
-    if args.iterations is not None:
-        n_iterations = args.iterations
-
-    # number of episodes per training step
-    n_eps_per_update = 25
-    if args.episodes is not None:
-        n_eps_per_update = args.episodes
-
-    # length of episode
-    n_max_steps = 200
-    if args.episode_length is not None:
-        n_max_steps = args.episode_length
-
-    # discount factor (gamma)
-    discount_factor = 0.99
-    if args.gamma is not None:
-        discount_factor = args.gamma
-
+    # OpenAI Gym setup
     if args.openai_gym:
-        # OpenAI Gym setup
-        n_iterations = 150
-        n_eps_per_update = 10
-        n_max_steps = 200
-        discount_factor = 0.95
+        hypers['iterations'] = 150
+        hypers['episodes per iteration'] = 10
+        hypers['steps per episode'] = 200
+        hypers['discount'] = 0.95
 
     threshold_factor = 0.9995
-    if args.variance == 0:
+    if hypers['variance'] == 0:
         threshold_factor = 0.94
-    elif args.variance == 1:
+    elif hypers['variance'] == 1:
         threshold_factor = 0.90
-    elif args.variance == 2:
+    elif hypers['variance'] == 2:
         threshold_factor = 0.60
 
     optimizer = keras.optimizers.Adam(lr=0.01)
     loss_fn = keras.losses.binary_crossentropy
 
     total_reward_progress = []
-    i = 0
     smash_counter = 0
-    best_score = n_max_steps*n_eps_per_update*threshold_factor*0.90
+
+    total_steps = hypers['steps per episode'] * hypers['episodes per iteration']
+    best_score = total_steps * threshold_factor * 0.90
+
+    if args.continue_:
+        best_score = old_best
+
+    print('Start training ...')
+    print('Using following hyperparameters:')
+    for key, value in hypers.items():
+        print(f'\t{key}: {value}')
+    print('--------------------------------')
+
+    i_best = 0
     best_weights = None
-    for iteration in range(n_iterations):
+    for i in range(hypers['iterations']):
         all_rewards, all_grads, best_ep_reward = play_multiple_episodes(
-            env, n_eps_per_update, n_max_steps, model, loss_fn,
-            gym=args.openai_gym
+            env, hypers['episodes per iteration'], hypers['steps per episode'],
+            model, loss_fn, gym=args.openai_gym
         )
         all_final_rewards = discount_and_normalize_rewards(all_rewards,
-                                                           discount_factor)
+                                                           hypers['discount'])
 
         flat_all_rewards = [i for ep_rewards in all_rewards for i in ep_rewards]
         total_iter_reward = float(sum(flat_all_rewards))
@@ -159,43 +193,53 @@ def main():
         optimizer.apply_gradients(
             zip(all_mean_grads, model.trainable_variables)
         )
-        i += 1
 
         if total_iter_reward > best_score:
-            print('< < best score, keeping weights > >')
+            print('< < best score, keeping weights, backup model > >')
             best_weights = model.get_weights()
             best_score = total_iter_reward
+            i_best = i
+            model.save(f"_BAK_{best_score}_at_i{i_best}.h5")
 
-        if total_iter_reward > n_max_steps*n_eps_per_update*threshold_factor:
+        if total_iter_reward > total_steps*threshold_factor:
             smash_counter += 1
 
-        print(f'done iter {iteration+1} of {n_iterations} \
+        print(f"done iter {i+1} of {hypers['iterations']} \
               - G_t={total_iter_reward} (G_best_ep: {best_ep_reward}) \
-              <score: {best_score}> ({smash_counter})')
+              <score: {best_score}> ({smash_counter})")
 
         if smash_counter > 5:
             break
 
     jsondumb = total_reward_progress
 
-    if args.variance == 0:
+    if hypers['variance'] == 0:
         v_str = 'low'
-    elif args.variance == 1:
+    elif hypers['variance'] == 1:
         v_str = 'med'
-    elif args.variance == 2:
+    elif hypers['variance'] == 2:
         v_str = 'high'
 
     if args.openai_gym:
         inputs_str = '4'
         annshape_str = '5'
     else:
-        inputs_str = args.inputs
-        annshape_str = ''.join(args.shape)
+        if args.continue_ is None:
+            inputs_str = args.inputs
+            annshape_str = ''.join(args.shape)
+        if args.continue_:
+            inputs_str, annshape_str = hypers['info'][0], hypers['info'][1:]
+
+    _disc = hypers["discount"]
+    _steps = hypers["steps per episode"]
+    _eps = hypers["episodes per interation"]
     modelname = f'pg_{v_str}_ref{int(args.reference)}_R{int(args.adv_reward)}' \
-                + f'_{inputs_str}{annshape_str}_g{int(discount_factor*100)}' \
-                + f'_s{n_max_steps}_eps{n_eps_per_update}_i{i}'
+                + f'_{inputs_str}{annshape_str}_g{int(_disc*100)}' \
+                + f'_s{_steps}_eps{_eps}_i{i_best+old_i}' \
+                + f'_Gt{round(best_score)}'
+
     if args.openai_gym:
-        modelname = f'pg_CartPole_{inputs_str}{annshape_str}_i{i}'
+        modelname = f'pg_CartPole_{inputs_str}{annshape_str}_i{i_best+old_i}'
 
     if smash_counter > 0:
         modelname = modelname + f'({smash_counter})'
